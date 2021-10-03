4feec5483da488c3af0f9f6d7fe43002419dbcf0
Jiri Laza <jiri.laza@gmail.com>
2021-10-03 18:27:07 +0200

    Fixed a bug with compiling JS files

--- lib/article_crud/article.ex
+++ lib/article_crud/article.ex
@@ -1,6 +1,5 @@
 defmodule Article.Article do
   alias ArticleCrud.EctoGenOutput.DbContext
-  alias ArticleCrud.EctoGenOutput.Models.GetArticlesItem
 
   def get_articles do
     case DbContext.get_articles() do
@@ -30,17 +29,6 @@ defmodule Article.Article do
     DbContext.delete_article(String.to_integer(id))
   end
 
-
-  def change_article(article) do
-    data  = article
-    types = %{}
-    params = %{}
-    changeset =
-      {data, types}
-      |> Ecto.Changeset.cast(params, Map.keys(types))
-  end
-
-  @spec create_article(binary, binary) :: {:error, any} | {:ok, [boolean]}
   def create_article(title, body) do
     DbContext.insert_article(title, body)
   end
